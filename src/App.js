207bc9b8e43d0c67fca22e1b11bfb8279f35d54a
Abdulaziz Erel <azizerel69@gmail.com>
2020-03-18 23:50:47 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LeftPanel from './components/leftPanel.js';
+import RightPanel from './components/rightPanel.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <LeftPanel />
+        <RightPanel />
+      </div>
     </div>
   );
 }
---
+++ src/components/rightPanel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '.././App.css';
+
+function RightPanel() {
+  return (
+
+<div className = "rightPanel">
+  <div className="rightContent">
+      <div className="rightHeader">
+        <h4> Let's Prepare For Lunch!</h4>
+        <p> Getting Started is quick, just fill out the into below </p>
+      </div>
+      <div className="rightBody">
+
+        <input type="text" placeholder ="Name"/>
+
+        <input type="text" placeholder="Email Adress"/>
+
+        <input type="text" placeholder="Password"/>
+
+        <input type="checkbox" id="terms" name="terms"/>
+        <label for="terms"> I agree to </label><label for="terms" id="termConditions">Terms & Conditions</label>
+      </div>
+  </div>
+</div>
+);
+}
+export default RightPanel;
